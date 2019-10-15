9d1991eb81c03b4442eea4ec75d342d1df403f8d
manojpanta <manojpanta95@gmail.com>
2019-10-14 18:47:32 -0600

    use send mail service to send emails

--- app/controllers/sendmail_controller.rb
+++ app/controllers/sendmail_controller.rb
@@ -1,6 +1,12 @@
 class SendmailController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
+    SendMailService.new.send_email(email_params)
     redirect_to root_path
+  end
+
+  private
+  def email_params
+    params.permit(:name, :name1, :email, :message)
   end
 end
