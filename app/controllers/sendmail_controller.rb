6b3848b42ef9c0dbf31e8d57c880271d371ecec4
manojpanta <manojpanta95@gmail.com>
2019-11-09 10:04:12 -0600

    added description for the method in sendmail controller

--- app/controllers/sendmail_controller.rb
+++ app/controllers/sendmail_controller.rb
@@ -3,5 +3,7 @@ class SendmailController < ApplicationController
   def create
     MailSender.contact_us(params[:name], params[:name1], params[:email],params[:message])
     redirect_to root_path
+
+    # this method sends email using sendgrid
   end
 end
